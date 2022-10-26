e0e6464bbf4c0e406826197041a7727d1b44b3b1
Julien Galeuzzi <Julien@MacBook-Pro-de-Rafael.local>
2022-10-26 10:01:57 +0200

    wildfire tracker project

---
+++ src/components/Map.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
+import LocationInfo from "./LocationInfo";
+
+const Map = ({ eventData, center, zoom }) => {
+  const [locationInformation, setLocationInformation] = useState(null);
+
+  const markers = eventData.map((e) => {
+    if (e.categories[0].id === "wildfires") {
+      return (
+        <LocationMarker
+          lat={e.geometry[0].coordinates[1]}
+          lng={e.geometry[0].coordinates[0]}
+          onClick={() => setLocationInformation({ id: e.id, title: e.title })}
+        />
+      );
+    }
+    return null;
+  });
+
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAJmK8j40h_5iuZtflV5KZ0kVD-K3QYiUk" }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        {markers}
+      </GoogleMapReact>
+      {locationInformation && <LocationInfo info={locationInformation} />}
+    </div>
+  );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 41.5381,
+    lng: -2.4447,
+  },
+  zoom: 5,
+};
+
+export default Map;
